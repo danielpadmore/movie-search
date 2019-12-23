d1f71d8f1918b8fd800984aab9510ffde219f497
Daniel Padmore <daniel.jack.padmore@gmail.com>
2019-12-23 14:24:17 +0000

    Added styling for search results

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -2,10 +2,23 @@ import React from "react";
 import classes from "./card.module.scss";
 
 type Props = {
+  backgroundUrl?: string;
   children: React.ReactNode;
 };
-const Card = ({ children }: Props) => {
-  return <div className={classes.container}>{children}</div>;
+const Card = ({ children, backgroundUrl }: Props) => {
+  const backgroundStyle = backgroundUrl
+    ? {
+        backgroundImage: `url(${backgroundUrl})`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover"
+      }
+    : {};
+  return (
+    <div style={{ ...backgroundStyle }} className={classes.container}>
+      {children}
+    </div>
+  );
 };
 
 export default Card;
--- src/components/search-result/search-result.tsx
+++ src/components/search-result/search-result.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { Movie } from "@danielpadmore/movie-client/dist/models/movie";
+import classes from "./search-result.module.scss";
 import Card from "../card/card";
 type Props = {
   movie: Movie;
 };
-const SearchResult = ({ movie }: Props) => (
-  <Card>
-    <h5>{movie.title}</h5>
-  </Card>
-);
+const SearchResult = ({ movie }: Props) => {
+  const releaseYear = new Date(movie.releaseDate).getFullYear();
+  return (
+    <Card backgroundUrl={movie.backdropUrl}>
+      <div className={classes.content}>
+        <p>
+          {movie.title} ({releaseYear})
+        </p>
+      </div>
+    </Card>
+  );
+};
 
 export default SearchResult;

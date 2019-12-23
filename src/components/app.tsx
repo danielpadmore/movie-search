43280d07f318ebf4149ae4a79d15ecb5a23e3635
Daniel Padmore <daniel.jack.padmore@gmail.com>
2019-12-23 12:10:13 +0000

    Created a hook to access the movie client. Added a custom hook to debounce a value, used initially for search term.

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,26 +1,26 @@
-import React, { useEffect, useState } from "react";
-import MovieDBClient from "@danielpadmore/movie-client/dist/movie-db-client";
-import { Movie } from "@danielpadmore/movie-client/dist/models/movie";
+import React, { useState } from "react";
 import { Router, Route, Switch } from "react-router";
 import SearchRoute from "../routes/search";
 import MovieRoute from "../routes/movie";
 import { createBrowserHistory } from "history";
 
-const movieClient = new MovieDBClient(process.env.MOVIE_DB_ACCESS_TOKEN || "");
+import { useMovieSearch } from "../hooks/use-movie-client";
+
 const history = createBrowserHistory();
 
 export const App = () => {
-  const [movies, setMovies] = useState<null | Movie[]>(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, searchLoading, searchError] = useMovieSearch(searchTerm);
 
-  useEffect(() => {
-    (async () => {
-      const results = await movieClient.searchAllMovies("iron man");
-      setMovies(results);
-    })();
-  }, []);
   return (
     <main>
       <h1>Movie Search</h1>
+      <input
+        value={searchTerm}
+        onChange={event => {
+          setSearchTerm(event.target.value);
+        }}
+      />
       <Router history={history}>
         <Switch>
           <Route path="/search" component={SearchRoute} />
